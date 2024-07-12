5792b0f58bd0c930c133ad5596466adbf2684fd4
markusknuts <markus.knuts@icloud.com>
2024-07-12 21:42:50 +0200

    Made function for humans choice

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let arr = ['Rock', 'Paper', 'Scissors'];
-
 function getComputerChoice() {
+    let arr = ['Rock', 'Paper', 'Scissors'];
     console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt('Choose between Rock, paper or Scissors!')
+}
+
+getHumanChoice()
