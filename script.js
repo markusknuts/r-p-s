be7c7f281a35d42ed8c0b2b4a7ed4bf0a37f15d7
markusknuts <markus.knuts@icloud.com>
2024-07-13 14:13:40 +0200

    Made the functions for computers choice and humans choice

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 function getComputerChoice() {
-    let arr = ['Rock', 'Paper', 'Scissors'];
-    console.log(arr[Math.floor(Math.random() * arr.length)]);
-}
+    let choice = Math.floor(Math.random() * 1000);
+    
+    if (choice <= 333) {
+        console.log('Rock!');
+    } else if (choice >= 334 && choice <= 666) {
+        console.log('Paper!');
+    } else if (choice >= 667) {
+        console.log('Scissors!');
+    }
+};
 
 function getHumanChoice() {
-    let humanChoice = prompt('Choose between Rock, paper or Scissors!')
-}
+    let choice2 = prompt('Choose between Rock, Paper or Scissors!');
+    if (choice2.toLowerCase() === 'rock') {
+        choice2 = 0;
+    } else if (choice2.toLowerCase() === 'paper') {
+        choice2 = 1;
+    } else if (choice2.toLowerCase() === 'scissors') {
+        choice2 = 2;
+    } else {
+        console.log('Wrong input. Reload the page and try again!');
+    }
+};
 
-getHumanChoice()
+getHumanChoice();
