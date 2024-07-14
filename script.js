35c0bf67737afc39b756751a07cdc3a5a241a60b
Markus Knuts <markus.knuts@icloud.com>
2024-07-14 23:13:15 +0200

    Finished the ui for Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function getHumanChoice() {
-    while (true) { // keeps loop going until valid response is input
-    let answer = prompt("Let's play Rock, Paper, Scissors! Please choose one of the following.").toLowerCase(); //case-insensitive prompt to user
-    if (answer == "rock" || answer == "paper" || answer == "scissors") { // conditional to allow user to only input the given options
-        return answer;
-    } else {
-        alert('Invalid choice! Please choose either "rock", "paper", or "scissors".');
-    }
-}
-}
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorBtn = document.querySelector("#scissors");
+const outcomeDiv = document.querySelector(".outcome")
+const playerScoreSpan = document.querySelector(".player-score")
+const computerScoreSpan = document.querySelector(".computer-score")
+
+let compScore = 0;
+let humanScore = 0;
+
+
 // randomly chooses an option for the computer 
 function getComputerChoice() {
 const choices = ['rock', 'paper', 'scissors']; // set variables in array for computer 
@@ -16,34 +17,85 @@ return choices[randomIndex]; // return the random index of choices
 }
 
 
-function playGame() { // Set main function to run the game and set score variables to 0
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) { // use two parameters (humchoice and comchoice) as arguments for conditionals 
-    if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-        humanScore += 1; //conditional if the human wins, log a string and add one point 
-    } else if (computerChoice == "rock" && humanChoice == "scissors" || computerChoice == "paper" && humanChoice == "rock" || computerChoice == "scissors" && humanChoice == "paper") {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        computerScore += 1; // conditional if the computer wins, log a string and add one point 
-    } else {
-        console.log("It's a tie!") // if neither conditionals are true, log a string (its a tie)
+const playRound = (playerSelection, computerSelection) => {
+    
+    if (playerSelection === computerSelection) {
+        const p = document.createElement("p")
+        p.textContent = 'it is a tie'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
+        const p = document.createElement("p")
+        p.textContent = 'You loose! Paper beats Rock'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        humanScore++
+        const p = document.createElement("p")
+        p.textContent = 'Yoy Win! Rock beats Scissors'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        humanScore++
+        const p = document.createElement("p")
+        p.textContent = 'You win! Paper beats Rock'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
+        const p = document.createElement("p")
+        p.textContent = 'You loose Scissors beats Paper'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
+        const p = document.createElement("p")
+        p.textContent = 'Yoy loose Rock beats Scissors'
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        humanScore++
+        const p = document.createElement("p")
+        p.textContent = 'Yoy win! Scissors beats Paper'
+        outcomeDiv.appendChild(p)
     }
+
+    
 }
 
-while (humanScore < 5 && computerScore < 5) { // 'while' loop to keep game going until one of the scores reaches 5 
-    playRound(getHumanChoice(), getComputerChoice());
+const checkForWinner = (playerScore, computerScore) => {
+    if (playerScore === 5) {
+        const h2 = document.createElement("h2")
+        h2.innerText = "You Won"
+        outcomeDiv.append(h2)
+    } else if (computerScore === 5) {
+        const h2 = document.createElement("h2")
+        h2.innerText = "You lost!"
+        outcomeDiv.append(h2)
+    }
+
 }
 
-if (humanScore = 5) { // conditional to end the game once either human or computer score reaches 5 
-    console.log("You beat the computer! " + humanScore + " to " + computerScore);
-} else if (computerScore = 5) {
-    console.log("The computer beat you! " + computerScore + " to " + humanScore);
-} else {
-    console.log("Nice round! Keep it going!");
+const updateScores = (humanScore, computerScore) => {
+    playerScoreSpan.innerText = `Player score: ${humanScore}`
+    computerScoreSpan.innerText = `Computer score: ${computerScore}`
 }
 
-}
+rockBtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'rock';
+    playRound(humanSelection, computerSelection)
+    updateScores(humanScore, compScore)
+    checkForWinner(humanScore, compScore)
+})
 
-playGame();
+paperBtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'paper';
+    playRound(humanSelection, computerSelection)
+    updateScores(humanScore, compScore)
+    checkForWinner(humanScore, compScore)
+})
+
+scissorBtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'scissors';
+    playRound(humanSelection, computerSelection)
+    updateScores(humanScore, compScore)
+    checkForWinner(humanScore, compScore)
+})
